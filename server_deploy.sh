cb653aba3939813ff693fdd459323ee7c1eeddde
Mohit Malviya <52241067+mohitmalviya786@users.noreply.github.com>
2022-09-11 01:45:30 +0530

    Update server_deploy.sh

--- server_deploy.sh
+++ server_deploy.sh
@@ -7,7 +7,7 @@ set -e
 echo "Deploying application ..."
 
 # Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+(php artisan down --status 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
     git pull origin master
 # Exit maintenance mode
